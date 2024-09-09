c79f6fbee54993c67a29fed10c3bd5a4bb5f956d
Carissa Nicholls <carissanicholls@hotmail.com>
2024-09-09 15:28:36 +0100

    if statements & loops

--- if statement.py
+++ if statement.py
@@ -7,3 +7,11 @@ elif n == 2:
     n *=2 
 else: 
     n += 2 
+
+    #Parentheses needed for multi-line conditions 
+    #and = && 
+    #or = || 
+    n, m = 1, 2 
+    if ((n > 2 and 
+         n ! = m) or n == m):
+         n += 1 
